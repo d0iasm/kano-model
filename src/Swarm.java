305ef67345c460522272f73016f3a28d4747194f
d0iasm <doiasami1219@gmail.com>
2018-11-20 00:55:36 +0900

    Move public functions

--- src/Swarm.java
+++ src/Swarm.java
@@ -55,6 +55,191 @@ public class Swarm extends JPanel {
         }
     }
 
+    public void run() {
+        double sumX, sumY;
+        double dis;
+        double diffX, diffY;
+        double paramK;
+        double rungeSumX, rungeSumY;
+
+        double tmpX, tmpY;
+
+        List<Double> newX = new ArrayList<>(pNum);
+        List<Double> newY = new ArrayList<>(pNum);
+
+//        double[][] kSums = new double[][]{
+//                {0, 0, 0},
+//                {0, 0, 0},
+//                {0, 0, 0},
+//        };
+
+        for (Particle p1 : particles) {
+            sumX = 0;
+            sumY = 0;
+
+            for (Particle p2 : particles) {
+                if (p1 == p2) continue;
+
+                // TODO: Calculate a distance table before this.
+                switch (boundary) {
+                    case OPEN:
+                        // dis: |Rij|.
+                        // {diffX, diffY}: Rij.
+                        dis = distance(p1, p2);
+                        diffX = diffX(p1, p2);
+                        diffY = diffY(p1, p2);
+                        break;
+                    case PERIODIC:
+                        // dis: |Rij|.
+                        // {diffX, diffY}: Rij.
+                        dis = distanceClosest(p1, p2);
+                        diffX = diffXClosest(p1, p2);
+                        diffY = diffYClosest(p1, p2);
+                        break;
+                    default:
+                        dis = distance(p1, p2);
+                        diffX = diffX(p1, p2);
+                        diffY = diffY(p1, p2);
+                }
+
+                // paramK: kij.
+                paramK = getKParam(p1.id, p2.id);
+
+                // TODO: Bug? |Rij|^-1 and |Rij|^-2
+//                sumX += (diffX(p1, p2) / dis) * (paramK * Math.pow(dis, -1.0) - Math.pow(dis, -2.0));
+//                sumY += (diffY(p1, p2) / dis) * (paramK * Math.pow(dis, -1.0) - Math.pow(dis, -2.0));
+//                tmpX = (diffX(p1, p2) / dis);
+//                tmpY = (diffY(p1, p2) / dis);
+
+                tmpX = (diffX / dis) * (paramK * Math.pow(dis, -0.8) - (1 / dis));
+                tmpY = (diffY / dis) * (paramK * Math.pow(dis, -0.8) - (1 / dis));
+
+//                kSums[(p1.id - 1) / pPartition][(p2.id - 1) / pPartition] = tmpX + tmpY;
+
+                sumX += tmpX;
+                sumY += tmpY;
+            }
+
+            rungeSumX = calcRungeKutta(sumX);
+            rungeSumY = calcRungeKutta(sumY);
+
+            newX.add(p1.x + rungeSumX);
+            newY.add(p1.y + rungeSumY);
+        }
+
+        for (int i = 0; i < pNum; i++) {
+            switch (boundary) {
+                case OPEN:
+                    particles.get(i).x = newX.get(i);
+                    particles.get(i).y = newY.get(i);
+                    break;
+                case PERIODIC:
+                    particles.get(i).x = imaging(newX.get(i));
+                    particles.get(i).y = imaging(newY.get(i));
+                    break;
+                default:
+                    particles.get(i).x = newX.get(i);
+                    particles.get(i).y = newY.get(i);
+            }
+        }
+
+//        flipKParamHeider(kSums);
+//        paramManager.changeKParamHeider(kSums);
+        this.params = paramManager.getParams();
+//        balanceKParamHeider(kSums);
+
+        count++;
+        if (count % 100 == 0) {
+            repaint();
+
+            if (count % 1000 == 0) {
+                System.out.println(paramManager.getParamChangedCount());
+                paramManager.setParamChangedCount(0);
+            }
+            if (count % 5000 == 0) {
+                printSwarmParam();
+                if (count == 50000) {
+                    printSwarmParam();
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    public void printSwarmParam() {
+        System.out.println("Print current kParams---------------------------");
+        System.out.println("count: " + count);
+        for (int i = 0; i < pType; i++) {
+            for (int j = 0; j < pType; j++) {
+                System.out.print(params[i][j] + ", ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println("Print current kParams---------------------------");
+    }
+
+    public void printSwarmParam(double[][] params) {
+        System.out.println("Print current kParams---------------------------");
+        System.out.println("count: " + count);
+        for (int i = 0; i < pType; i++) {
+            for (int j = 0; j < pType; j++) {
+                System.out.print(params[i][j] + ", ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println("Print current kParams---------------------------");
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.LIGHT_GRAY);
+        switch (boundary) {
+            case OPEN:
+                for (int i = 0; i < h; i += (10 * scale)) {
+                    g2.drawLine(0, i, w, i);
+                    g2.drawLine(i, 0, i, h);
+                }
+                break;
+            case PERIODIC:
+                Stroke def = g2.getStroke();
+                Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+                g2.setStroke(dashed);
+                g2.drawLine(0, h / 2, w, h / 2);
+                g2.drawLine(w / 2, 0, w / 2, h);
+                g2.setStroke(def);
+                break;
+        }
+
+        for (Particle p : particles) {
+            if (p.id <= pPartition) {
+                g2.setColor(Color.RED);
+            } else if (pPartition < p.id && p.id <= pPartition * 2) {
+                g2.setColor(Color.BLUE);
+            } else {
+                g2.setColor(Color.GREEN);
+            }
+
+            switch (boundary) {
+                case OPEN:
+                    g2.fill(new Ellipse2D.Double(
+                            p.x * scale,
+                            p.y * scale,
+                            pSize * scale, pSize * scale));
+                case PERIODIC:
+                    g2.fill(new Ellipse2D.Double(
+                            p.x * scale * 8,
+                            p.y * scale * 8,
+                            pSize * scale, pSize * scale));
+
+            }
+        }
+    }
+
+
+    // ------------------- Private --------------------
     private void showParams() {
         this.setLayout(null);
         this.add(paramManager.getTitle());
@@ -212,188 +397,5 @@ public class Swarm extends JPanel {
         double k3 = x + k2 * timeStep * 0.5;
         double k4 = x + k3 * timeStep;
         return (k1 + 2 * k2 + 2 * k3 + k4) * (timeStep / 6.0);
-    }
-
-    public void run() {
-        double sumX, sumY;
-        double dis;
-        double diffX, diffY;
-        double paramK;
-        double rungeSumX, rungeSumY;
-
-        double tmpX, tmpY;
-
-        List<Double> newX = new ArrayList<>(pNum);
-        List<Double> newY = new ArrayList<>(pNum);
-
-//        double[][] kSums = new double[][]{
-//                {0, 0, 0},
-//                {0, 0, 0},
-//                {0, 0, 0},
-//        };
-
-        for (Particle p1 : particles) {
-            sumX = 0;
-            sumY = 0;
-
-            for (Particle p2 : particles) {
-                if (p1 == p2) continue;
-
-                // TODO: Calculate a distance table before this.
-                switch (boundary) {
-                    case OPEN:
-                        // dis: |Rij|.
-                        // {diffX, diffY}: Rij.
-                        dis = distance(p1, p2);
-                        diffX = diffX(p1, p2);
-                        diffY = diffY(p1, p2);
-                        break;
-                    case PERIODIC:
-                        // dis: |Rij|.
-                        // {diffX, diffY}: Rij.
-                        dis = distanceClosest(p1, p2);
-                        diffX = diffXClosest(p1, p2);
-                        diffY = diffYClosest(p1, p2);
-                        break;
-                    default:
-                        dis = distance(p1, p2);
-                        diffX = diffX(p1, p2);
-                        diffY = diffY(p1, p2);
-                }
-
-                // paramK: kij.
-                paramK = getKParam(p1.id, p2.id);
-
-                // TODO: Bug? |Rij|^-1 and |Rij|^-2
-//                sumX += (diffX(p1, p2) / dis) * (paramK * Math.pow(dis, -1.0) - Math.pow(dis, -2.0));
-//                sumY += (diffY(p1, p2) / dis) * (paramK * Math.pow(dis, -1.0) - Math.pow(dis, -2.0));
-//                tmpX = (diffX(p1, p2) / dis);
-//                tmpY = (diffY(p1, p2) / dis);
-
-                tmpX = (diffX / dis) * (paramK * Math.pow(dis, -0.8) - (1 / dis));
-                tmpY = (diffY / dis) * (paramK * Math.pow(dis, -0.8) - (1 / dis));
-
-//                kSums[(p1.id - 1) / pPartition][(p2.id - 1) / pPartition] = tmpX + tmpY;
-
-                sumX += tmpX;
-                sumY += tmpY;
-            }
-
-            rungeSumX = calcRungeKutta(sumX);
-            rungeSumY = calcRungeKutta(sumY);
-
-            newX.add(p1.x + rungeSumX);
-            newY.add(p1.y + rungeSumY);
-        }
-
-        for (int i = 0; i < pNum; i++) {
-            switch (boundary) {
-                case OPEN:
-                    particles.get(i).x = newX.get(i);
-                    particles.get(i).y = newY.get(i);
-                    break;
-                case PERIODIC:
-                    particles.get(i).x = imaging(newX.get(i));
-                    particles.get(i).y = imaging(newY.get(i));
-                    break;
-                default:
-                    particles.get(i).x = newX.get(i);
-                    particles.get(i).y = newY.get(i);
-            }
-        }
-
-//        flipKParamHeider(kSums);
-//        paramManager.changeKParamHeider(kSums);
-        this.params = paramManager.getParams();
-//        balanceKParamHeider(kSums);
-
-        count++;
-        if (count % 100 == 0) {
-            repaint();
-
-            if (count % 1000 == 0) {
-                System.out.println(paramManager.getParamChangedCount());
-                paramManager.setParamChangedCount(0);
-            }
-            if (count % 5000 == 0) {
-                printSwarmParam();
-//                if (count == 50000) {
-//                    printSwarmParam();
-//                    System.exit(0);
-//                }
-            }
-        }
-    }
-
-    public void printSwarmParam() {
-        System.out.println("Print current kParams---------------------------");
-        System.out.println("count: " + count);
-        for (int i = 0; i < pType; i++) {
-            for (int j = 0; j < pType; j++) {
-                System.out.print(params[i][j] + ", ");
-            }
-            System.out.println(" ");
-        }
-        System.out.println("Print current kParams---------------------------");
-    }
-
-    public void printSwarmParam(double[][] params) {
-        System.out.println("Print current kParams---------------------------");
-        System.out.println("count: " + count);
-        for (int i = 0; i < pType; i++) {
-            for (int j = 0; j < pType; j++) {
-                System.out.print(params[i][j] + ", ");
-            }
-            System.out.println(" ");
-        }
-        System.out.println("Print current kParams---------------------------");
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-
-        g2.setColor(Color.LIGHT_GRAY);
-        switch (boundary) {
-            case OPEN:
-                for (int i = 0; i < h; i += (10 * scale)) {
-                    g2.drawLine(0, i, w, i);
-                    g2.drawLine(i, 0, i, h);
-                }
-                break;
-            case PERIODIC:
-                Stroke def = g2.getStroke();
-                Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
-                g2.setStroke(dashed);
-                g2.drawLine(0, h / 2, w, h / 2);
-                g2.drawLine(w / 2, 0, w / 2, h);
-                g2.setStroke(def);
-                break;
-        }
-
-        for (Particle p : particles) {
-            if (p.id <= pPartition) {
-                g2.setColor(Color.RED);
-            } else if (pPartition < p.id && p.id <= pPartition * 2) {
-                g2.setColor(Color.BLUE);
-            } else {
-                g2.setColor(Color.GREEN);
-            }
-
-            switch (boundary) {
-                case OPEN:
-                    g2.fill(new Ellipse2D.Double(
-                            p.x * scale,
-                            p.y * scale,
-                            pSize * scale, pSize * scale));
-                case PERIODIC:
-                    g2.fill(new Ellipse2D.Double(
-                            p.x * scale * 8,
-                            p.y * scale * 8,
-                            pSize * scale, pSize * scale));
-
-            }
-        }
     }
 }
