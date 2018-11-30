c990cec1be6b42dcc72419d798b2708351f0005f
d0iasm <doiasami1219@gmail.com>
2018-11-30 11:30:05 +0900

    Update class to package private

--- src/Extension.java
+++ src/Extension.java
@@ -1,10 +1,10 @@
-public final class Extension {
-    public static void printSwarmParam(double[][] params, int count) {
+final class Extension {
+    static void printSwarmParam(double[][] params, int count) {
         System.out.println("----------- Print current kParams --------------");
         System.out.println("count: " + count);
-        for (int i = 0; i < params.length; i++) {
+        for (double[] param : params) {
             for (int j = 0; j < params[0].length; j++) {
-                System.out.print(params[i][j] + ", ");
+                System.out.print(param[j] + ", ");
             }
             System.out.println(" ");
         }
