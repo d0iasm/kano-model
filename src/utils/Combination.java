4e99e5635fb413d7e8c273d9ef39bee0a4a22da5
d0iasm <doiasami1219@gmail.com>
2018-12-07 15:14:07 +0900

    Hide combination implement from outside a class

--- src/utils/Combination.java
+++ src/utils/Combination.java
@@ -1,31 +1,76 @@
 package utils;
 
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Combination {
-    public static void main(String[] args) {
-        System.out.println(combination(3, 5));
+    // Reference: https://rosettacode.org/wiki/Combinations#Java
+
+    private int n, k;
+    private List<int[]> comb = new ArrayList<>();
+
+    public Combination(int n, int k) {
+        if (n < k || n < 1) {
+            throw new IllegalArgumentException();
+        }
+        this.n = n;
+        this.k = k;
+        comb = combination();
     }
 
-    public static String bitprint(int u) {
-        String s = "";
+    public int size() {
+        return comb.size();
+    }
+
+    public List<int[]> list() {
+        return comb;
+    }
+
+    public void print() {
+        for (int[] i : comb) {
+            for (int j=0; j<3; j++) {
+                System.out.print(i[j] + ", ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    private int[] findOne(int u) {
+        // TODO: Consider this method name.
+        int r[] = new int[u];
+        int idx = 0;
         for (int n = 0; u > 0; ++n, u >>= 1)
-            if ((u & 1) > 0) s += n + " ";
-        return s;
+            if ((u & 1) > 0) {
+                r[idx] = n;
+                idx++;
+            }
+        return r;
     }
 
-    public static int bitcount(int u) {
+    private int bitcount(int u) {
         int n;
-        for (n = 0; u > 0; ++n, u &= (u - 1)) ;//Turn the last set bit to a 0
+        for (n = 0; u > 0; ++n, u &= (u - 1)) ;
         return n;
     }
 
-    public static LinkedList<String> combination(int c, int n) {
-        LinkedList<String> s = new LinkedList<String>();
-        for (int u = 0; u < 1 << n; u++)
-            if (bitcount(u) == c) s.push(bitprint(u));
-        Collections.sort(s);
-        return s;
+    private List<int[]> combination() {
+        for (int u = 0; u < 1 << n; u++) {
+            System.out.println(u);
+            if (bitcount(u) == k) comb.add(findOne(u));
+        }
+        return comb;
+    }
+
+    public static void main(String[] args) {
+        Combination c = new Combination(4, 3);
+        List<int[]> l = c.list();
+        System.out.println("The number of combinations is " + c.size());
+        for (int[] i : l) {
+            for (int j=0; j<3; j++) {
+                System.out.print(i[j] + ", ");
+            }
+            System.out.println(" ");
+        }
     }
 }
