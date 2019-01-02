df2d15cf964fcaf6618c6fbff3ea1564ff4f92c3
d0iasm <doiasami1219@gmail.com>
2019-01-01 21:18:10 -0800

    Add new parameter for X and V

--- src/KaKmKp.java
+++ src/KaKmKp.java
@@ -1,9 +1,25 @@
 import java.math.BigDecimal;
 
+/**
+ * Parameters in this class are defined in Kano's thesis(Mathematical Analysis for Non-reciprocal-interaction-based Model of Collective Behavior, 2017).
+ */
 public class KaKmKp extends Parameter {
-    private final BigDecimal a = new BigDecimal(0.7); // This value is defined in Kano's thesis(2017).
-    private final BigDecimal p = new BigDecimal(0.9); // This value is defined in Kano's thesis(2017).
+    private final BigDecimal a = new BigDecimal(0.7);
+    private final BigDecimal p = new BigDecimal(0.9);
     private BigDecimal m;
+    
+    /**
+     *  X = (N^(-1) * Σ(N, i=1)|ri - rg|)^(-1)
+     *  rg = N^(-1) * Σ(N, i=1)ri
+     *  rg denotes the position of the center of gravity.
+     *  X converges to zero when at least one of the particles moves an infinite distance from the center of gravity.
+     */
+    private BigDecimal x;
+    /**
+     * V = N^(-1) * Σ(N, i=1)|ri(dot)-rg(dot)|
+     * V converges to zero when the relative velocities of all particles with respect to the center of gravity converge to zero.
+     */
+    private BigDecimal v;
 
     KaKmKp(int dimension) {
         super(dimension);
